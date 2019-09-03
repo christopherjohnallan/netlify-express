596d84bf55cacecec16afab264a2ee58ee9de347
Chris Allan <christopher.john.allan@gmail.com>
2019-09-02 21:43:44 -0500

    Add pwa dynamic manifest

--- express/server.js
+++ express/server.js
@@ -10,8 +10,33 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/manifest.json', (req, res) => {
+  // You can dynamically generate your manifest here
+  // You can pull the data from database and send it back
+  // I will use a template for simplicity
+
+  //Use some logic to extract organization name from referer
+  var matches = /\/([a-z]+)\/?$/i.exec(req.headers.referer);
+  if (matches && matches.length > 1) {
+    var orgName = matches[1];
+  } else {
+    var orgName = 'ORGA'; //Default
+  }
+
+  // Need to set content type, default is text/html
+  res.set('Content-Type', 'application/json');
+  res.render('manifest.hbs', { orgName });
+});
+
+app.get('/:orgName', (req, res) => {
+  res.render('index.hbs', { orgName: req.params.orgName });
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
