20b5d4e3fd2fac68119b348d5969aa9be884c5c2
Chris Allan <christopher.john.allan@gmail.com>
2019-09-02 21:58:11 -0500

    testing

--- express/server.js
+++ express/server.js
@@ -7,45 +7,11 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>HelloQ from Express.js!</h1>');
+  res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-/* app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
-
-router.get('/manifest.json', (req, res) => {
-  // You can dynamically generate your manifest here
-  // You can pull the data from database and send it back
-  // I will use a template for simplicity
-
-  //Use some logic to extract organization name from referer
-  var matches = /\/([a-z]+)\/?$/i.exec(req.headers.referer);
-  if (matches && matches.length > 1) {
-    var orgName = matches[1];
-  } else {
-    var orgName = 'ORGA'; //Default
-  }
-
-  // Need to set content type, default is text/html
-  res.set('Content-Type', 'application/json');
-  res.render('manifest.hbs', { orgName });
-}); */
-
-/* router.get('/:orgName', (req, res) => {
-  // res.render('index.hbs', { orgName: req.params.orgName });
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>BOO!</h1>');
-  res.end();
-}); */
-
-/* router.get('/boo', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Boo!</h1>');
-  res.end();
-}); */
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
